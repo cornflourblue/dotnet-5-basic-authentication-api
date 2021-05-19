ff843379ca7a5909d53a5157aaff6c6d7decaaee
Jason Watmore <cornflourblue@hotmail.com>
2021-05-19 20:24:46 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotnet-5-basic-authentication-api
+
+.NET 5.0 - Basic HTTP Authentication API
+
+For documentation and instructions check out https://jasonwatmore.com/post/2021/05/19/net-5-basic-authentication-tutorial-with-example-api
---
+++ Services/UserService.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Entities;
+
+namespace WebApi.Services
+{
+    public interface IUserService
+    {
+        Task<User> Authenticate(string username, string password);
+        Task<IEnumerable<User>> GetAll();
+    }
+
+    public class UserService : IUserService
+    {
+        // users hardcoded for simplicity, store in a db with hashed passwords in production applications
+        private List<User> _users = new List<User>
+        {
+            new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" }
+        };
+
+        public async Task<User> Authenticate(string username, string password)
+        {
+            // wrapped in "await Task.Run" to mimic fetching user from a db
+            var user = await Task.Run(() => _users.SingleOrDefault(x => x.Username == username && x.Password == password));
+
+            // return null if user not found
+            if (user == null)
+                return null;
+
+            // authentication successful so return user details
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            // wrapped in "await Task.Run" to mimic fetching users from a db
+            return await Task.Run(() => _users);
+        }
+    }
+}
